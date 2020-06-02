ff3f4c840b3c3306344c3bc0dbbce4840e31553f
andre347 <aedevries19@gmail.com>
2020-06-02 19:21:42 +0200

    kata completed

---
+++ enumerablemagic.js
@@ -0,0 +1,34 @@
+function one(arr, fun) {
+  // ...
+  // place to hold the status
+  let result = arr.map(fun).filter((item) => item === true).length == 1;
+  return result;
+}
+
+console.log(
+  one([1, 2, 3, 4, 5], function (item) {
+    return item < 2;
+  }),
+  true
+);
+console.log(
+  one([1, 2, 3, 4, 5], function (item) {
+    return item % 2;
+  }),
+  false,
+  "more than 1 item are odd"
+);
+console.log(
+  one([1, 2, 3, 4, 5], function (item) {
+    return item > 5;
+  }),
+  false,
+  "none item is greater than 5"
+);
+console.log(
+  one([4, 5, 1, 8, 9, 3, 6, 4], function (item) {
+    return item === 4;
+  }),
+  false,
+  "2 items equals 5"
+);
